b12ce39746faf4103484db49c260f8045f4eea04
FARAHrafah123 <farahrahali9876@gmail.com>
2023-03-21 16:17:36 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
 * print_alphabet - print all alphabet in lowercase
-*I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+*I sometimes suffer from insomnia
+*I play what I call the alphabet game
 */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
