5e71dd653281dc8151600ee439fa0a36b3ccfb6f
FARAHrafah123 <farahrahali9876@gmail.com>
2023-12-13 02:04:57 -0800

    103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,40 +1,66 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - Searches for a value in a sorted array
- * using Interpolation search
- *
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
- * @value: The value to search for
- *
- * Return: The first index where the value is located, or -1 if not found
- */
-int interpolation_search(int *array, size_t size, int value)
+  * _binary_search - Searches for a value in a sorted array
+  *                  of integers using binary search.
+  * @array: POINTER TO THE ARRAY HAS OUR ELEMENTS
+  * @left: THE LEFT PART OF OUR SUBARRAY
+  * @right: THE RIGHT PART OF OUR SUBARRAY
+  * @value: THE VALUE TO SEARCH FOR
+  *
+  * Return: NULL IF NOT FOUND, -1.
+  *         Otherwise, THE INDEDEX WHERE WE FOUND THE ELEMENT.
+  */
+int _binary_search(int *array, size_t left, size_t right, int value)
 {
-	size_t low = 0;
-	size_t high = size - 1;
-	size_t pos;
+	size_t n;
 
-	while (array)
+	if (array == NULL)
+		return (-1);
+
+	while (right >= left)
 	{
-		pos = low + (((double)(high - low) / (
-						array[high] - array[low])) * (value - array[low]));
+		printf("Searching in array: ");
+		for (n = left; n < right; n++)
+			printf("%d, ", array[n]);
+		printf("%d\n", array[n]);
 
-		if (pos < size)
-			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		n = left + (right - left) / 2;
+		if (array[n] == value)
+			return (n);
+		if (array[n] > value)
+			right = n - 1;
 		else
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			return (-1);
-		}
-
-		if (value == array[pos])
-			return (pos);
-		else if (array[pos] < value)
-			low = pos + 1;
-		else
-			high = pos - 1;
+			left = n + 1;
 	}
+
 	return (-1);
+}
+
+/**
+  * exponential_search - Searches for a value in a sorted array
+  *                      of integers using exponential search.
+  * @array: POINTER TO THE ARRAY HAS OUR ELEMENTS
+  * @size: NUMBER OF ELEMENTS IN THE ARRAY.
+  * @value: THE VALUE TO SEARCH FOR
+  *
+  * Return:  NULL IF NOT FOUND, -1.
+  *         Otherwise, THE INDEDEX WHERE WE FOUND THE ELEMENT.
+  */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t f = 0, right;
+
+	if (array == NULL)
+		return (-1);
+
+	if (array[0] != value)
+	{
+		for (f = 1; f < size && array[f] <= value; f = f * 2)
+			printf("Value checked array[%ld] = [%d]\n", f, array[f]);
+	}
+
+	right = f < size ? f : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", f / 2, right);
+	return (_binary_search(array, f / 2, right, value));
 }
